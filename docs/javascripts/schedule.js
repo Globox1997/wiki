a6201e63d6cff27c1f0143afc8f007e35812235a
Globox1997 <mitschke11@web.de>
2023-06-20 18:47:34 +0200

    updated schedule

--- docs/javascripts/schedule.js
+++ docs/javascripts/schedule.js
@@ -1,3 +1,5 @@
+const KEY = process.argv[2];
+
 var projectDownloadsMap = new Map();
 var totalDownloadCount = 0;
 
@@ -10,18 +12,47 @@ fetch(`https://api.modrinth.com/v2/user/Globox1997/projects`, {
         projectDownloadsMap.set(obj.slug, obj.downloads);
         totalDownloadCount += obj.downloads;
     }, 0);
-}).catch(error => {
-    console.error('Error:', error);
 }).then(data => {
     projectDownloadsMap.set('total', totalDownloadCount);
-    writeFile(projectDownloadsMap);
+    writeFile(projectDownloadsMap, "modrinth");
+}).catch(error => {
+    console.error('Error:', error);
 });
 
-function writeFile(data) {
+const inputBody = {
+    "modIds": [
+        390991, 486971, 693762, 682362, 864150, 659920, 481182, 508695, 533879, 404828, 429383, 365273, 480911, 472795, 666011, 532113, 410830, 551914, 480786, 411621, 402739, 862603, 517130, 841617, 410224, 411400, 834671, 615948, 424480, 736803, 666672, 703778, 615504, 534368, 398269, 396541
+    ]
+};
+
+projectDownloadsMap.clear();
+totalDownloadCount = 0;
+
+fetch('https://api.curseforge.com/v1/mods', {
+    method: 'POST',
+    body: JSON.stringify(inputBody),
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'x-api-key': KEY
+    }
+}).then(response => response.json()).then(data => {
+    data.data.reduce((acc, obj) => {
+        projectDownloadsMap.set(obj.slug, obj.downloadCount);
+        totalDownloadCount += obj.downloadCount;
+    }, 0);
+}).then(data => {
+    projectDownloadsMap.set('total', totalDownloadCount);
+    writeFile(projectDownloadsMap, "curseforge");
+}).catch(error => {
+    console.error('Error:', error);
+});
+
+function writeFile(data, platform) {
     const fs = require('fs');
     const path = require('path');
 
-    const filePath = path.join(__dirname, '../data/collected.json');
+    const filePath = path.join(__dirname, '../data/' + platform + '_collected.json');
 
     const plainObject = Object.fromEntries(data);
     const jsonString = JSON.stringify(plainObject);
