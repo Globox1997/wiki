8768e1188632ce62a39e5c2af82693a8e0f0cb9a
Globox1997 <mitschke11@web.de>
2023-09-16 17:44:39 +0200

    tweaked gallery

--- docs/javascripts/gallery.js
+++ docs/javascripts/gallery.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
     iconGallery();
+    resourceGallery();
 });
 
 function iconGallery() {
@@ -23,7 +24,6 @@ function iconGallery() {
             { texture: 'jobsaddon_icon', url: 'JobsAddon', tooltip: 'JobsAddon' },
             { texture: 'levelz_icon', url: 'LevelZ', tooltip: 'LevelZ' },
             { texture: 'libz_icon', url: 'LibZ', tooltip: 'LibZ' },
-            { texture: 'medievalmusic_icon', url: 'MedievalMusic', tooltip: 'MedievalMusic' },
             { texture: 'medievalweapons_icon', url: 'MedievalWeapons', tooltip: 'MedievalWeapons' },
             { texture: 'nameplate_icon', url: 'Nameplate', tooltip: 'Nameplate' },
             { texture: 'numismaticclaim_icon', url: 'NumismaticClaim', tooltip: 'NumismaticClaim' },
@@ -31,19 +31,43 @@ function iconGallery() {
             { texture: 'patchoulibutton_icon', url: 'PatchouliButton', tooltip: 'PatchouliButton' },
             { texture: 'reciperemover_icon', url: 'RecipeRemover', tooltip: 'RecipeRemover' },
             { texture: 'revive_icon', url: 'Revive', tooltip: 'Revive' },
+            { texture: 'rewardz_icon', url: 'RewardZ', tooltip: 'RewardZ' },
             { texture: 'rpgdifficulty_icon', url: 'RpgDifficulty', tooltip: 'RpgDifficulty' },
             { texture: 'rpgloot_icon', url: 'RpgZ', tooltip: 'RpgZ' },
             { texture: 'spoiled_icon', url: 'SpoiledZ', tooltip: 'SpoiledZ' },
             { texture: 'talkbubbles_icon', url: 'TalkBubbles', tooltip: 'TalkBubbles' },
             { texture: 'tiered_icon', url: 'TieredZ', tooltip: 'TieredZ' },
             { texture: 'tooltiprareness_icon', url: 'TooltipRareness', tooltip: 'TooltipRareness' },
+            { texture: 'travelerz_icon', url: 'TravelerZ', tooltip: 'TravelerZ' },
             { texture: 'villagerquests_icon', url: 'VillagerQuests', tooltip: 'VillagerQuests' },
             { texture: 'villagertradefix_icon', url: 'VillagerTradeFix', tooltip: 'VillagerTradeFix' },
-            { texture: 'voidz_icon', url: 'VoidZ', tooltip: 'VoidZ' }
+            { texture: 'voidz_icon', url: 'VoidZ', tooltip: 'VoidZ' },
+            { texture: 'welcomescreen_icon', url: 'WelcomeScreen', tooltip: 'WelcomeScreen' }
         ];
 
         // Get the gallery element
         const gallery = document.getElementById('icon-gallery');
+
+        // Create and append the icons to the gallery
+        icons.forEach((iconData) => {
+            const icon = document.createElement('img');
+            icon.src = '/wiki/assets/general/icons/' + iconData.texture + '.png';
+            icon.className = 'icon';
+
+            tooltip(icon, iconData.tooltip, iconData.url, true);
+            gallery.appendChild(icon);
+        });
+    }
+}
+
+function resourceGallery() {
+    if (document.getElementById('resource-gallery') != null) {
+        const icons = [
+            { texture: 'medievalmusic_icon', url: 'MedievalMusic', tooltip: 'MedievalMusic' }
+        ];
+
+        // Get the gallery element
+        const gallery = document.getElementById('resource-gallery');
 
         // Create and append the icons to the gallery
         icons.forEach((iconData) => {
--- docs/mods/index.md
+++ docs/mods/index.md
@@ -6,3 +6,8 @@ summary: Globox_Z's all mods wiki
 
 <div id="icon-gallery"></div>
 <script src="/wiki/javascripts/gallery.js"></script>
+
+
+---
+# Resource Pack
+<div id="resource-gallery"></div>
