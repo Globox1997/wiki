8cc7c1a9790b612b0bb763d9b9f3bdd8d626fc1d
Globox1997 <mitschke11@web.de>
2024-10-03 16:26:03 +0200

    custom texture

--- docs/javascripts/info.js
+++ docs/javascripts/info.js
@@ -50,13 +50,66 @@ function list() {
     }
 }
 
+// duplicated at overview.js
 function getCustomTexture(texture) {
     switch (texture) {
         case 'blackstones_arm':
             return 'blackstone_golem_arm';
+        case 'wooden_battle_axe':
+            return 'wooden_small_axe';
+        case 'wooden_longsword':
+            return 'wooden_long_sword';
+        case 'wooden_staff_of_healing':
+            return 'wooden_healing_staff';
+        case 'wooden_war_axe':
+            return 'wooden_big_axe';
+
+        case 'stone_battle_axe':
+            return 'stone_small_axe';
+        case 'stone_longsword':
+            return 'stone_long_sword';
+        case 'stone_staff_of_healing':
+            return 'stone_healing_staff';
+        case 'stone_war_axe':
+            return 'stone_big_axe';
+
+        case 'golden_battle_axe':
+            return 'golden_small_axe';
+        case 'golden_longsword':
+            return 'golden_long_sword';
+        case 'golden_staff_of_healing':
+            return 'golden_healing_staff';
+        case 'golden_war_axe':
+            return 'golden_big_axe';
+
+        case 'iron_battle_axe':
+            return 'iron_small_axe';
+        case 'iron_longsword':
+            return 'iron_long_sword';
+        case 'iron_staff_of_healing':
+            return 'iron_healing_staff';
+        case 'iron_war_axe':
+            return 'iron_big_axe';
+
+        case 'diamond_battle_axe':
+            return 'diamond_small_axe';
+        case 'diamond_longsword':
+            return 'diamond_long_sword';
+        case 'diamond_staff_of_healing':
+            return 'diamond_healing_staff';
+        case 'diamond_war_axe':
+            return 'diamond_big_axe';
+
+        case 'netherite_battle_axe':
+            return 'netherite_small_axe';
+        case 'netherite_longsword':
+            return 'netherite_long_sword';
+        case 'netherite_staff_of_healing':
+            return 'netherite_healing_staff';
+        case 'netherite_war_axe':
+            return 'netherite_big_axe';
 
         default:
             return null;
     }
 }
-
